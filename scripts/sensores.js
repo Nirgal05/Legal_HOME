e8e0c9b615f6662b2116d1b19f7f2994eba0ee18
sanyul <sanyul@me.com>
2019-12-11 20:11:01 -0600

    Desarrollé la página de lista de sensores LEGALHOME

---
+++ scripts/sensores.js
@@ -0,0 +1,11 @@
+
+
+function activate(id) {
+    let element = document.getElementById(id);
+    element.innerHTML = "Activado";
+}
+
+function deactivate(id) {
+    let element = document.getElementById(id);
+    element.innerHTML = "Desactivado";
+}
