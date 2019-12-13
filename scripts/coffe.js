b3c15301fd0449f46bb658feca02577d7c7506dc
Nirgal05 <elontesla@gmail.com>
2019-12-12 19:29:14 -0600

    Version beta, sólo falta agregar el logo,4

--- scripts/coffe.js
+++ scripts/coffe.js
@@ -1,13 +1,27 @@
-const cafe={
-    nivelWater:"Optimo",
-    TempRange:0,
-    HoraCafe:"00:00",
+const cafe = {
+    alarmActiva: "Alarma: ACTIVA",
+    alarmINActiva: "Alarma: INACTIVA",
+    nivelWater: "Optimo", //El nivel de agua va ligado a un sensor
+    TempRange: {
+        barra: function(){
+            let Temperatures = document.getElementById("TempRange");
+            document.getElementById("TempSelect").innerHTML = Temperatures.value+" °C"; //Temperatura seleccionada
+        }
+    },
     botones: {
-        onCafe: function(){
-
+        onCafe: function () {
+            let alarmCafe = document.getElementById("alarmActiva");
+            alarmCafe.innerHTML = cafe.alarmActiva; //Status de alarma café
+            let alarmas = document.getElementById("HoraSelect"); 
+            document.getElementById("HoraCafe").innerHTML = alarmas.value; //Hora del café
         },
-        offCafe: function(){
-            //
+        offCafe: function () {
+            let alarmCafe = document.getElementById("alarmActiva");
+            alarmCafe.innerHTML = cafe.alarmINActiva; //Alarma desactivada
+            let alarmas = document.getElementById("HoraSelect");
+            document.getElementById("HoraCafe").innerHTML = "OFF"; //Hora de café desactivada
         }
     }
-}
+};
+let nivelWaterCafe = document.getElementById("nivelWater");
+document.getElementById("nivelWater").innerHTML = cafe.nivelWater;
